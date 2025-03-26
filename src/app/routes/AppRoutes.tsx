afedf7df6de2c4fddfdcc7ddcec83ce42bfc305f
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2025-03-26 11:17:55 +0300

    Поправил роуты

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -1,4 +1,9 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { LayoutWithSidebar } from "@ui/LayoutWithSidebar/LayoutWithSidebar";
 import { Sidebar } from "@components/Sidebar/Sidebar";
 import { ProductDetails } from "@components/ProductDetails/ProductDetails";
@@ -7,18 +12,20 @@ import { EditProduct } from "@components/EditProduct/EditProduct";
 import { ProductList } from "@components/ProductList/ProductList";
 
 export const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<Navigate to="/products" replace />} />
-    <Route
-      path="/products"
-      element={
-        <LayoutWithSidebar sidebar={<Sidebar />}>
-          <ProductList />
-        </LayoutWithSidebar>
-      }
-    />
-    <Route path="/product/:id" element={<ProductDetails />} />
-    <Route path="/create-product" element={<CreateProduct />} />
-    <Route path="/edit-product/:id" element={<EditProduct />} />
-  </Routes>
+  <Router basename="/productCards">
+    <Routes>
+      <Route path="/" element={<Navigate to="/products" replace />} />
+      <Route
+        path="/products"
+        element={
+          <LayoutWithSidebar sidebar={<Sidebar />}>
+            <ProductList />
+          </LayoutWithSidebar>
+        }
+      />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/create-product" element={<CreateProduct />} />
+      <Route path="/edit-product/:id" element={<EditProduct />} />
+    </Routes>
+  </Router>
 );
