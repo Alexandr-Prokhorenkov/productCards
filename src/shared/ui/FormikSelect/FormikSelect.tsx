dd411ebc8dd67b58cee5ce1d8f0bce03d257e9fd
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2025-03-27 09:01:50 +0300

    Поправил баг

--- src/shared/ui/FormikSelect/FormikSelect.tsx
+++ src/shared/ui/FormikSelect/FormikSelect.tsx
@@ -8,20 +8,24 @@ interface FormikSelectProps {
   options: { label: string; value: string }[];
 }
 
-export const FormikSelect: React.FC<FormikSelectProps> = ({ label, options, ...props }) => {
-  const [field, meta, helpers] = useField(props);
+export const FormikSelect: React.FC<FormikSelectProps> = ({
+  label,
+  options,
+  ...props
+}) => {
+  const [field, , helpers] = useField(props);
 
-  const selectedValue = typeof field.value === 'object' ? JSON.stringify(field.value) : field.value;
+  const selectedValue =
+    typeof field.value === "object" ? JSON.stringify(field.value) : field.value;
 
   return (
     <div className={styles.fieldWrapper}>
       <Select
         label={label}
         options={options}
-        value={selectedValue || ""} 
+        value={selectedValue || ""}
         onChange={(value) => helpers.setValue(value)}
       />
-      {meta.touched && meta.error && <div className={styles.error}>{meta.error}</div>}
     </div>
   );
 };
